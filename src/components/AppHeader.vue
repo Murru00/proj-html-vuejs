8d3425e8a4ddae586579334fca20b66442902c7d
Murru00 <marcomurruolbia@gmail.com>
2024-02-20 11:08:09 +0100

    add link and icon header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+import { store } from '../store/index';
+
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    link: 'Home',
+                },
+                {
+                    link: 'Landing',
+                },
+                {
+                    link: 'Pages',
+                }, 
+                {
+                    link: 'Docs',
+                }
+
+
+            ],
+            icons: [
+                {
+                    iconName: 'fa-brands fa-github'
+                },
+                {
+                    iconName: 'fa-solid fa-book-open'
+                },
+                {
+                    iconName: 'fa-regular fa-user'
+                }
+            ],
+            store,
+        };
+    }
+};
+</script>
