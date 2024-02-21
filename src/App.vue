21d61825a79fba844680c0293613918391d8a508
Murru00 <marcomurruolbia@gmail.com>
2024-02-21 13:15:42 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
-<script>
-import { store } from './store';
-import axios from 'axios';
 
-// IMPORTIAMO LE APP 
-
-import AppMain from './components/AppMain.vue';
-import AppFooter from './components/AppFooter.vue';
-import AppHeader from './components/AppHeader.vue';
-import AppCard from './components/AppCard.vue';
+<script></script>
 
 
+<template>
+  <header>
+    <div id="container">
+      <div class="left-logo">
+        <img src="../src/assets/img/logo-dark.png" alt="" class="head-logo">
+      </div>
+      <div class="head-link">
+        <ul>
+          <li>HOME<i class="fa-solid fa-check"></i></li>
+          <li>LANDING<i class="fa-solid fa-check"></i></li>
+          <li>PAGES<i class="fa-solid fa-check"></i></li>
+          <li>DOCS<i class="fa-solid fa-check"></i></li>
+        </ul>
+      </div>
+      <div class="icon-link">
+       <ul>
+        <li class="icon-search"><i class="fa-solid fa-magnifying-glass"></i></li>
+        <li class="right-icon"><i class="fa-brands fa-github"></i></li>
+        <li class="right-icon"><i class="fa-solid fa-book-open-reader"></i></li>
+        <li class="right-icon"><i class="fa-regular fa-user"></i></li>
+       </ul>
+      </div>
+    </div>
+  </header>
+  <main>
+    
+  </main>
+  <footer></footer>
+</template>
 
-export default {
-  data() {
-    return {
 
-    };
-  },
 
-  // COMPONENTS
+<style>
+.head-logo{
+  width: 170px;
+}
 
-  components: { AppHeader, AppMain, AppCard, AppFooter },
-};
+.left-logo{
+  margin-top: 35px;
+}
 
-</script>
+ul{
+  display: flex;
+  gap: 30px;
+  list-style-type: none;
+  margin-top: 40px;
+  cursor: pointer;
+}
+
+#container{
+  display: flex;
+  justify-content: space-around;
+}
+
+.right-icon{
+  background-color: rgb(180, 177, 177);
+  color: rgb(60, 3, 251);
+  border: 2px solid rgb(180, 177, 177);
+  border-radius: 3px;
+  font-size:20px;
+  
+}
+
+.icon-search{
+  color: grey;
+  
+}
+
+
+</style>
